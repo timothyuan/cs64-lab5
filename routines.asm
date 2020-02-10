d1fd288d4a6e4d824cf19a2629ba3ab0a8c89dfd
Timothy Yuan <timothyyuan@cs.ucsb.edu>
2020-02-09 22:45:07 -0800

    routines

--- routines.asm
+++ routines.asm
@@ -7,15 +7,46 @@
 
 .text
 routineB:
-    # TODO: Write your function code here
+	addi $a0, $a0, -5
+	li $t4, 4
+	mult $a0, $t4
+	mflo $v0
+	jr $ra
 
 routineA:
-    # TODO: Write your function code here
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	move $t0, $a0
+	move $t1, $a0
+	li $t2, 2
+	mult $t0, $t2
+	mflo $s1
+
+	li $t3, 3
+	mult $a0, $t3
+	mflo $a0
+	jal routineB
+
+	add $s1, $s1, $v0
+	move $a0, $s1
+	addi $a0, -1
+	jal routineB
+
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	jr $ra	
 
 main:
-
-	# TODO: Write your main function code here
+	li $a0, 5
+	li $a1, 7
+	jal routineA
+	move $s0, $v0
+	li $v0, 1
+	move $a0, $v0
+	syscall
 
 exit:
-	# TODO: Write code to properly exit a SPIM simulation
+	li $v0, 10
+	syscall
 
+	
