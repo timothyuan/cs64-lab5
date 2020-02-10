dd2d22a08557c4a623aab35218ac309c02165bf4
Timothy Yuan <timothyyuan@cs.ucsb.edu>
2020-02-09 21:39:41 -0800

    conversion

--- conversion.asm
+++ conversion.asm
@@ -7,12 +7,42 @@
 
 .text
 conv:
-    # TODO: Write your function code here
+	li $t0, 0
+	li $t1, 0
+	li $t2, 5
+	li $t3, 2
 
+loop:
+	mult $a0, $t3
+	mflo $t4
+	add $t0, $t0, $t4
+	sub $t0, $t0, $a1
+	blt $t3, $a0, func
+	addi $a0, $a0, 1
+	addi $t1, $t1, 1
+	beq $t1, $t2, return
+	j loop
+
+func:
+	addi $a1, $a1, -1
+	addi $a0, $a0, 1
+	addi $t1, $t1, 1
+	beq $t1, $t2, return
+	j loop
+
+
+return:
+	move $v0, $t0
+	jr $ra	
+	
 main:
-
-	# TODO: Write your main function code here
+	li $a0, 5
+	li $a1, 7
+	jal conv
+	move $a0, $v0
+	li $v0, 1
+	syscall
 
 exit:
-	# TODO: Write code to properly exit a SPIM simulation
-
+	li $v0, 10
+	syscall
